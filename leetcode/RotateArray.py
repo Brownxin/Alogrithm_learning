43ac353bda255705d81692440f558ad1575359e9
Xin Wang <kevin.wx86@gmail.com>
2015-09-22 18:02:39 -0400

    Alogrithm

--- leetcode/RotateArray.py
+++ leetcode/RotateArray.py
@@ -5,23 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        # Solution 1
-        if nums==None or len(nums)==1:
-            return nums
-        temp=[]
-        length=len(nums)
-        m=n=(abs(length-k))%length
+# Solution 1
+        # if nums==None or len(nums)==1:
+        #     return nums
+        # temp=[]
+        # length=len(nums)
+        # m=n=(abs(length-k))%length
+        #
+        # for i in range(len(nums)):
+        #     if n>0:
+        #         temp.append(nums[i])
+        #         n-=1
+        #     else:
+        #         nums[i-m]=nums[i]
+        # index=0
+        # for i in range(m,len(nums)):
+        #     if index<len(nums):
+        #         nums[i]=temp[index]
+        #         index+=1
+        # return nums
 
-        for i in range(len(nums)):
-            if n>0:
-                temp.append(nums[i])
-                n-=1
-            else:
-                nums[i-m]=nums[i]
-        index=0
-        for i in range(m,len(nums)):
-            if index<len(nums):
-                nums[i]=temp[index]
-                index+=1
+#Solution 2
+        n=len(nums)
+        nums[:]=nums[n-k:]+nums[0:n-k]
         return nums
 
