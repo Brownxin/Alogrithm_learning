5a2cf2f1758da5482795f40e298790fce93a876b
Xin Wang <kevin.wx86@gmail.com>
2015-09-22 17:44:28 -0400

    Alogrithm-test

---
+++ leetcode/testRotateArray.py
@@ -0,0 +1,13 @@
+__author__ = 'Brown'
+
+import unittest
+from leetcode import RotateArray
+
+class MyTestCase(unittest.TestCase):
+    def testRotateArray(self):
+        # self.assertEqual(RotateArray.Solution().rotate([1],0),[1],'Fail' )
+        self.assertEqual(RotateArray.Solution().rotate([1,2],1),[2,1],'Fail' )
+
+
+if __name__ == '__main__':
+    unittest.main()
