6a62069fa6097295349abdc02dc6315fb6265616
Xin Wang <kevin.wx86@gmail.com>
2015-09-26 18:23:08 -0400

    Algorithm

--- leetcode/NextPermutation.py
+++ leetcode/NextPermutation.py
@@ -1,1 +1,29 @@
 __author__ = 'Brown'
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)<2:
+            return nums
+        p=-1
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                p=i
+                break
+        if p==-1:
+            nums.reverse()
+            return nums
+        else:
+            for i in range(len(nums)-1,p,-1):
+                if nums[i]>nums[p]:
+                    nums[i],nums[p]=nums[p],nums[i]
+                    break
+            left=p+1
+            right=len(nums)-1
+            while left<right:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+                right-=1
+            return nums
