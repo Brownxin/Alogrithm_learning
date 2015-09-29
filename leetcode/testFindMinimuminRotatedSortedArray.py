6154b34f3a39cead5d3dcebcd88d7630896763b7
Xin Wang <kevin.wx86@gmail.com>
2015-09-28 21:52:46 -0400

    Algorithm-test

--- leetcode/testFindMinimuminRotatedSortedArray.py
+++ leetcode/testFindMinimuminRotatedSortedArray.py
@@ -1,11 +1,11 @@
 __author__ = 'Brown'
 
 import unittest
-
+from leetcode import FindMinimuminRotatedSortedArray
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def testFindMinimuminRotatedSortedArray(self):
+        self.assertEqual(FindMinimuminRotatedSortedArray.Solution().findMin([4,5,6,7,0,1,2]),0,'fail')
 
 
 if __name__ == '__main__':
